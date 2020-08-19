d432720ba46494dd0278f8afd1acb2e4c4662aa2
Lama Alrweta <lamaalruwaita18@gmail.com>
2020-08-20 02:50:13 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -17,7 +17,7 @@ def main():
 
     @st.cache(persist=True)
     def load_data():
-        data = pd.read_csv("Mushroom.csv")
+        data = pd.read_csv("mushrooms.csv")
         labelencoder=LabelEncoder()
         for col in data.columns:
             data[col] = labelencoder.fit_transform(data[col])
